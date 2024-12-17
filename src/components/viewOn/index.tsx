e22f21fe768b7644513cb557d54d0c9cf9d6fd0a
yureribeiro <yureribeirocontato@gmail.com>
2024-12-16 22:40:08 -0300

    add new dx

--- src/components/viewOn/index.tsx
+++ src/components/viewOn/index.tsx
@@ -4,6 +4,7 @@ import dexscreen from '../../../public/dexscreener.png'
 import jupiter from '../../../public/jupiter.svg'
 import pumpfun from '../../../public/pumpfun.png'
 import bird from '../../../public/birdeye.png'
+import photon from '../../../public/photon.png'
 import Link from 'next/link';
 import danonIcon from '../../../public/logo.png'
 
@@ -12,16 +13,19 @@ export default function ViewOn() {
     <div className={s.container}>
       <div className={s.cards}>
         <Link href='https://dexscreener.com/' target='_blank' rel="noreferrer" className={s.link}>
-          <Image src={dexscreen} alt="dexscreener" className={s.icon} quality={100} priority />
+          <Image src={dexscreen} alt="Dexscreener" className={s.icon} quality={100} priority />
         </Link>
         <Link href='https://pump.fun/coin/38BWrTTU17xWpgQfYQRJYcAKw4qBnKta72yYLCuepump' target='_blank' rel="noreferrer" className={s.link}>
-          <Image src={pumpfun} alt="pumpfun" className={s.icon} quality={100} priority />
+          <Image src={pumpfun} alt="Pumpfun" className={s.icon} quality={100} priority />
         </Link>
         <Link href='https://jup.ag/swap/' target='_blank' rel="noreferrer" className={s.link}>
-          <Image src={jupiter} alt="jupiter" className={s.icon} quality={100} priority />
+          <Image src={jupiter} alt="Jupiter" className={s.icon} quality={100} priority />
         </Link>
         <Link href='https://www.birdeye.so/token/38BWrTTU17xWpgQfYQRJYcAKw4qBnKta72yYLCuepump?chain=solana' target='_blank' rel="noreferrer" className={s.link}>
-          <Image src={bird} alt="birdeye" className={s.icon} quality={100} priority />
+          <Image src={bird} alt="Birdeye" className={s.icon} quality={100} priority />
+        </Link>
+        <Link href='https://photon-sol.tinyastro.io/en/lp/HtGojj14bRnD7cNaUnWEAwj1PmYeG2EBNeCn9tkJ6nNd ' target='_blank' rel="noreferrer" className={s.link}>
+          <Image src={photon} alt="Photon" className={s.icon} quality={100} priority />
         </Link>
       </div>
       <p className={s.text}>Big exchanges lined up, just wait... <Image src={danonIcon} alt="logo" priority quality={100} className={s.image} /></p>
